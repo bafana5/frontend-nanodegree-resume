e460373977acd758e28f3d03fd2b820d646aa5fe
Meshack Shabalala <bafana5@gmail.com>
2017-12-26 01:01:20 +0200

    feat($personal-details): Added my name and current position to resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+var name      = "Meshack Shabalala";
+var role      = "Digital Video & Image Processing Research Engineer";
+var contacts  = ["0128414391", "mshabalala@csir.co.za", "bafana5@gmail.com"]
+var skills    = ["Java Programming", "C++", "Python"]
+
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contacts" : contacts,
+	"picture" : "pic",
+	"welcome" : "Hello out there",
+	"skills"  : skills
+};
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+$('#header').prepend(formattedName);
+$('#header').append(formattedRole);
+
+// console.log(firstName)
+// $("#main").append("Meshack Shabalala")
